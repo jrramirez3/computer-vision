16e696bbdb6001cd256f126e8b53c3d85b376e7e
roatienza <rowel.atienza@gmail.com>
2019-04-15 20:31:55 +0800

    base net

--- detection/ssd/model.py
+++ detection/ssd/model.py
@@ -15,33 +15,46 @@ from keras.models import load_model
 from keras.layers.merge import concatenate
 from keras.utils import plot_model
 
-def conv_layer(inputs,
-               name_postfix,
-               filters=16,
-               kernel_size=3,
-               strides=1,
-               activation=None):
+import argparse
+
+def conv2d(inputs,
+           filters=32,
+           kernel_size=3,
+           strides=1,
+           name=None):
 
     conv = Conv2D(filters=filters,
                   kernel_size=kernel_size,
                   strides=strides,
                   kernel_initializer='he_normal',
                   # kernel_regularizer=l2(l2_reg),
-                  name="conv"+name_postfix,
-                  use_maxpool=True,
+                  name=name,
                   padding='same')
 
-    x = inputs
-    x = conv(x)
-    x = BatchNormalization(name="bn"+name_postfix)(x)
-    x = ELU(name='elu'+name_postfix)(x)
+    return conv(inputs)
+
+
+def conv_layer(inputs,
+               filters=32,
+               kernel_size=3,
+               strides=1,
+               use_maxpool=True,
+               postfix=None,
+               activation=None):
+
+    x = conv2d(inputs,
+               kernel_size=kernel_size,
+               strides=strides,
+               name='conv'+postfix)
+    x = BatchNormalization(name="bn"+postfix)(x)
+    x = ELU(name='elu'+postfix)(x)
     if use_maxpool:
-        x = MaxPooling2D(name='pool'+name_postfix)(x)
+        x = MaxPooling2D(name='pool'+postfix)(x)
     return x
 
 def build_basenetwork(input_shape,
                       output_shape=None,
-                      name=None):
+                      name='base_network'):
 
     channels = int(output_shape[-1])
 
@@ -49,60 +62,92 @@ def build_basenetwork(input_shape,
     conv1 = conv_layer(inputs,
                        32,
                        kernel_size=5,
-                       name_postfix="1")
+                       postfix="1")
 
     conv2 = conv_layer(conv1,
                        48,
                        kernel_size=3,
-                       name_postfix="2")
+                       postfix="2")
 
     conv3 = conv_layer(conv2,
                        64,
                        kernel_size=3,
-                       name_postfix="3")
+                       postfix="3")
 
     conv4 = conv_layer(conv3,
                        64,
                        kernel_size=3,
-                       name_postfix="4")
+                       postfix="4")
     
     conv5 = conv_layer(conv4,
                        48,
                        kernel_size=3,
-                       name_postfix="5")
+                       postfix="5")
     
     conv6 = conv_layer(conv5,
                        48,
                        kernel_size=3,
-                       name_postfix="6")
+                       postfix="6")
     
     conv7 = conv_layer(conv6,
                        48,
                        kernel_size=3,
-                       name_postfix="7",
+                       postfix="7",
                        use_maxpool=False)
 
-
-    classes4 = Conv2D(n_boxes[0] * n_classes, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='classes4')(conv4)
-        classes5 = Conv2D(n_boxes[1] * n_classes, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='classes5')(conv5)
-            classes6 = Conv2D(n_boxes[2] * n_classes, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='classes6')(conv6)
-                classes7 = Conv2D(n_boxes[3] * n_classes, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='classes7')(conv7)
-                    # Output shape of `boxes`: `(batch, height, width, n_boxes * 4)`
-                        boxes4 = Conv2D(n_boxes[0] * 4, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='boxes4')(conv4)
-                            boxes5 = Conv2D(n_boxes[1] * 4, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='boxes5')(conv5)
-                                boxes6 = Conv2D(n_boxes[2] * 4, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='boxes6')(conv6)
-                                    boxes7 = Conv2D(n_boxes[3] * 4, (3, 3), strides=(1, 1), padding="same", kernel_initializer='he_normal', kernel_regularizer=l2(l2_reg), name='boxes7')(conv7)
-
-    basenetwork = Model(inputs, outputs, name=name)
+    basenetwork = Model(inputs, [conv4, conv5, conv6, conv7], name=name)
 
     return basenetwork
+
+
+def build_ssd(inputs,
+              basenetwork):
+              
+
+    conv4, conv5, conv6, conv7 = basenetwork(innputs)
+
+    classes4  = conv2d(conv4,
+                       n_boxes[0] * n_classes,
+                       kernel_size=3,
+                       name='classes4')
+    classes5  = conv2d(conv5,
+                       n_boxes[1] * n_classes,
+                       kernel_size=3,
+                       name='classes5')
+    classes6  = conv2d(conv6,
+                       n_boxes[2] * n_classes,
+                       kernel_size=3,
+                       name='classes6')
+    classes7  = conv2d(conv7,
+                       n_boxes[3] * n_classes,
+                       kernel_size=3,
+                       name='classes7')
+
+    # Output shape of `boxes`: `(batch, height, width, n_boxes * 4)`
+    boxes4  = conv2d(conv4,
+                     n_boxes[0] * 4,
+                     kernel_size=3,
+                     name='boxes4')
+    boxes5  = conv2d(conv5,
+                     n_boxes[1] * 4,
+                     kernel_size=3,
+                     name='boxes5')
+    boxes6  = conv2d(conv6,
+                     n_boxes[2] * 4,
+                     kernel_size=3,
+                     name='boxes6')
+    boxes7  = conv2d(conv7,
+                     n_boxes[3] * 4,
+                     kernel_size=3,
+                     name='boxes7')
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     help_ = "Build model"
     args = parser.parse_args()
-    input_shape = (256, 256, 1)
-    output_shape = (256, 256, 1)
+    input_shape = (480, 300, 3)
+    output_shape = (480, 300, 3)
     base = build_basenetwork(input_shape, output_shape)
     base.summary()
