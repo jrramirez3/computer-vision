c589d9893207469feaf5fe8376da11d70b2dde3f
Rowel Atienza <rowel.atienza@gmail.com>
2019-04-24 17:48:22 +0800

    tensor

--- detection/ssd/layer_utils.py
+++ detection/ssd/layer_utils.py
@@ -16,6 +16,7 @@ def anchor_boxes(x,
                  img_width,
                  this_scale,
                  aspect_ratios):
+    n_boxes = len(aspect_ratios)
     size = min(img_height, img_width)
     wh_list = []
     for ar in aspect_ratios:
@@ -30,8 +31,8 @@ def anchor_boxes(x,
     wh_list = np.array(wh_list)
 
     batch_size, feature_map_height, feature_map_width, feature_map_channels = K.int_shape(x)
-    step_height = self.img_height / feature_map_height
-    step_width = self.img_width / feature_map_width
+    step_height = img_height / feature_map_height
+    step_width = img_width / feature_map_width
     offset_height = 0.5
     offset_width = 0.5
         
@@ -52,21 +53,24 @@ def anchor_boxes(x,
     cy_grid = np.expand_dims(cy_grid, -1)
     # Create a 4D tensor template of shape `(feature_map_height, feature_map_width, n_boxes, 4)`
     # where the last dimension will contain `(cx, cy, w, h)`
-    boxes_tensor = np.zeros((feature_map_height, feature_map_width, self.n_boxes, 4))
-    boxes_tensor[:, :, :, 0] = np.tile(cx_grid, (1, 1, self.n_boxes)) # Set cx
-    boxes_tensor[:, :, :, 1] = np.tile(cy_grid, (1, 1, self.n_boxes)) # Set cy
+    boxes_tensor = np.zeros((feature_map_height, feature_map_width, n_boxes, 4))
+    boxes_tensor[:, :, :, 0] = np.tile(cx_grid, (1, 1, n_boxes)) # Set cx
+    boxes_tensor[:, :, :, 1] = np.tile(cy_grid, (1, 1, n_boxes)) # Set cy
     boxes_tensor[:, :, :, 2] = wh_list[:, 0] # Set w
     boxes_tensor[:, :, :, 3] = wh_list[:, 1] # Set h
     # Convert `(cx, cy, w, h)` to `(xmin, xmax, ymin, ymax)`
     # boxes_tensor = convert_coordinates(boxes_tensor, start_index=0, conversion='centroids2corners')
-    tensor = np.copy(boxes_tensor).astype(np.float)
-    tensor[..., 0] = boxes_tensor[..., 0] - boxes_tensor[..., 2] / 2.0 # Set xmin
-    tensor[..., 1] = boxes_tensor[..., 1] - boxes_tensor[..., 3] / 2.0 # Set ymin
-    tensor[..., 2] = boxes_tensor[..., 0] + boxes_tensor[..., 2] / 2.0 # Set xmax
-    tensor[..., 3] = boxes_tensor[..., 1] + boxes_tensor[..., 3] / 2.0 # Set ymax
-    boxes_tensor = tensor
+    boxes_tensor = centroid2corners(boxes_tensor)
     # Now prepend one dimension to `boxes_tensor` to account for the batch size and tile it along
     # The result will be a 5D tensor of shape `(batch_size, feature_map_height, feature_map_width, n_boxes, 8)`
     boxes_tensor = np.expand_dims(boxes_tensor, axis=0)
     boxes_tensor = K.tile(K.constant(boxes_tensor, dtype='float32'), (K.shape(x)[0], 1, 1, 1, 1))
     return boxes_tensor
+
+def centroid2corners(boxes_tensor):
+    tensor = np.copy(boxes_tensor).astype(np.float)
+    tensor[..., 0] = boxes_tensor[..., 0] - boxes_tensor[..., 2] / 2.0 # Set xmin
+    tensor[..., 1] = boxes_tensor[..., 1] - boxes_tensor[..., 3] / 2.0 # Set ymin
+    tensor[..., 2] = boxes_tensor[..., 0] + boxes_tensor[..., 2] / 2.0 # Set xmax
+    tensor[..., 3] = boxes_tensor[..., 1] + boxes_tensor[..., 3] / 2.0 # Set ymax
+    return tensor
