bdf25360d391bac0f6e4b722b2ab45d84c7dbc69
Rowel Atienza <rowel.atienza@gmail.com>
2019-05-02 14:58:02 +0800

    dic

--- detection/ssd/viz_boxes.py
+++ detection/ssd/viz_boxes.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 
 import numpy as np
 import skimage
+import csv
 from skimage.io import imread
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -24,10 +25,6 @@ def show_anchors(image, feature_shape, boxes):
     fig, ax = plt.subplots(1)
     # Display the image
     ax.imshow(image)
-    print(boxes[0][0][0][0])
-    print(boxes[0][0][0][1])
-    print(boxes[0][0][0][2])
-    print(boxes.shape)
     # Show grids
     grid_height = image_height // feature_height
     for i in range(feature_height):
@@ -55,9 +52,37 @@ def show_anchors(image, feature_shape, boxes):
             w = box[2]
             h = box[3]
             rect = Rectangle((x, y), w, h, linewidth=1, edgecolor=color, facecolor='none')
-            #rect = Rectangle((200, 100),100,30, linewidth=1, edgecolor='r', facecolor='none')
             ax.add_patch(rect)
     plt.show()
+
+def loadcsv(path):
+    data = []
+    with open(path) as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        # rows = rows[1:]
+        for row in rows:
+            data.append(row)
+
+    return np.array(data)
+    # return np.genfromtxt(path, dtype=[str, np.uint8, np.uint8, np.uint8, np.uint8, np.uint8], delimiter=',')
+
+def dict_label(labels, keys):
+    dic = {}
+    for key in keys:
+        boxes = []
+        dic[key] = boxes
+        print(key)
+    for label in labels:
+        value = label[1:]
+        key = label[0]
+        boxes = dic[key]
+        boxes.append(value)
+        dic[key] = boxes
+        print(boxes)
+
+    return dic
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -77,5 +102,15 @@ if __name__ == '__main__':
                          image.shape,
                          is_K_tensor=False)
 
-    show_anchors(image, feature_shape, boxes)
+    # show_anchors(image, feature_shape, boxes)
 
+    labels = loadcsv(args.csv)
+    labels = labels[1:]
+    print(labels.shape)
+    keys = np.unique(labels[:,0])
+    print(labels[1])
+    print(keys[1])
+    print(keys.shape)
+    dic = dict_label(labels, keys)
+    print(dic[keys[0]])
+    print(dic[keys[1]])
