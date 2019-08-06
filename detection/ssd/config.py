0bdc330f178106668574b8de560aef18b69f1abd
jrramirez3 <jane.ramirez@eee.upd.edu.ph>
2019-08-06 14:44:53 +0800

    config changes

--- detection/ssd/config.py
+++ detection/ssd/config.py
@@ -7,11 +7,11 @@ from __future__ import division
 from __future__ import print_function
 
 params = {
-        'data_path' : 'dataset/drinks',
+        'data_path' : 'dataset/dataSyn',
         'train_labels' : 'labels_train.csv',
         'test_labels' : 'labels_test.csv',
-        'n_classes' : 3,
-        'aspect_ratios': [1, 2, 0.5],
-        'classes' : ["background", "Summit", "Coke", "Pine Juice"],
-        'prices' : [0.0, 10.0, 40.0, 35.0]
+        'n_classes' : 2,
+        'aspect_ratios': [1, 1, 1],
+        'classes' : ["background", "Head"],
+        # 'prices' : [0.0, 10.0, 40.0, 35.0]
         }
