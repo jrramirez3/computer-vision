9e5b80bbe3fe7a32defe6740c6e6f3d41965dbcc
Rowel Atienza <rowel.atienza@gmail.com>
2019-08-19 09:46:33 +0800

    config

--- detection/ssd/config.py
+++ detection/ssd/config.py
@@ -7,16 +7,10 @@ from __future__ import division
 from __future__ import print_function
 
 params = {
-        'dataset' : 'heads',
-        'data_path' : 'dataset/heads',
+        'dataset' : 'drinks',
+        'data_path' : 'dataset/drinks',
         'train_labels' : 'labels_train.csv',
         'test_labels' : 'labels_test.csv',
-<<<<<<< HEAD
-        'epoch_offset': 30,
-        'n_classes' : 1,
-        'aspect_ratios': [1, 2, 0.5],
-        'classes' : ["background", "head"],
-=======
         'epoch_offset': 0,
         'aspect_ratios': [1, 2, 0.5],
         'class_thresh' : 0.8,
@@ -24,6 +18,5 @@ params = {
         'is_soft_nms' : True,
         'n_classes' : 3,
         'classes' : ["background", "Summit", "Coke", "Pine Juice"],
->>>>>>> 129845f0200b066c3e9de5925dfafb3850a5c2a0
         'prices' : [0.0, 10.0, 40.0, 35.0]
         }
